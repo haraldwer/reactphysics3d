0dc51d0b83289f4fd78c316f143b1cb74dfc7b86
Daniel Chappuis <chappuis.daniel@gmail.com>
2025-02-16 17:14:45 +0100

    Fix issue with wrong index condition in TriangleMesh::removeUnusedVertices() method

--- src/collision/TriangleMesh.cpp
+++ src/collision/TriangleMesh.cpp
@@ -217,7 +217,7 @@ bool TriangleMesh::copyData(const TriangleVertexArray& triangleVertexArray, std:
 void TriangleMesh::removeUnusedVertices(Array<bool>& areUsedVertices) {
 
     // For each vertex of the user mesh
-    for (uint32 i=mVertices.size() - 1; i > 0; i--) {
+    for (int64 i=mVertices.size() - 1; i >= 0; i--) {
 
         // If the vertex is not used
         if (!areUsedVertices[i]) {
